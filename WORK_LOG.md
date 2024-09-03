051baf8a6196e6aed92ad015fd9827cc40085e4c
Benjamin Chock <benjamin.chock@menloschool.org>
2024-09-03 12:30:41 -0700

    commit 1

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -1,9 +1,10 @@
 Name: YOUR NAME HERE
 
-| Date    |  Time  |                                     Update |
-|:--------|:------:|-------------------------------------------:|
-| April 5 | 9-10pm | I tried to write abstract, efficient code. |
-|         |        |                                            |
+| Date        |   Time    |                                                                                                                            Update |
+|:------------|:---------:|----------------------------------------------------------------------------------------------------------------------------------:|
+| April 5     |  9-10pm   |                                                                                        I tried to write abstract, efficient code. |
+| September 2 | 8:30-9:00 |                                                                              I tried to think of/brainstorm ways to solve problem |
+| Sebtember 2 |  9-9:30   | Started coding my idea/ solution: find the quickest path to highest ladder/ladder that gets the highest then go through that path |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/MokshaPatam.java
+++ src/MokshaPatam.java
@@ -4,7 +4,7 @@
  * for Adventures in Algorithms
  * at Menlo School in Atherton, CA
  *
- * Completed by: [YOUR NAME HERE]
+ * Completed by: Benjamin Chock
  *
  */
 
@@ -15,7 +15,43 @@ public class MokshaPatam {
      *  to reach the final square on a board with the given size, ladders, and snakes.
      */
     public static int fewestMoves(int boardsize, int[][] ladders, int[][] snakes) {
+        int totalmoves = 0;
+        int distance = 0;
+        int highestLadder = 0;
+        int currentplace = 0;
+        int nextGoal = highestLadder(ladders, currentplace);
+        int goal = highestLadder(ladders, currentplace, nextGoal);
 
-        return 0;
+
+        while (currentplace != boardsize){
+            totalmoves++;
+
+        }
+        return totalmoves;
     }
+    //gives you location of the square where the highest later is located
+    public int highestLadder(int[][] ladders, int limit){
+        int num = limit;
+        int goal = 0;
+        for (int i = 0; i < ladders[0].length; i++){
+            if (num < ladders[1][i]){
+                num = ladders[1][i];
+                goal = i;
+            }
+        }
+        return goal;
+
+
+    }
+    //gives you location of ladder closet to location of highest ladder
+    public int highestLadder(int[][] ladders, int limit, int nextGoal){
+        int num = limit;
+        int goal = 0;
+        for (int i = 0; i < ladders[0].length; i++){
+            if (num < ladders[1][i] && ladders[1][i] < nextGoal){
+                num = ladders[1][i];
+                goal = i;
+            }
+        }
+        return goal;
 }
